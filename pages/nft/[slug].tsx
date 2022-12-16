f69411db33fe33798cd27447102a0e40af3c51c0
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-12-16 19:45:43 -0300

    chore: created responsive main content element

--- pages/nft/[slug].tsx
+++ pages/nft/[slug].tsx
@@ -20,7 +20,35 @@ function NFTDropPage() {
           </div>
         </div>
       </aside>
-      <div></div>
+      <main className="flex flex-1 flex-col p-12 lg:col-span-6">
+        <header className="flex items-center justify-between">
+          <h1 className="w-52 cursor-pointer text-xl font-extralight sm:w-80">
+            The{' '}
+            <span className="font-extrabold underline decoration-pink-600/50">
+              PAPAFAM
+            </span>{' '}
+            NFT Market Place
+          </h1>
+          <button className="rounded-full bg-rose-400 text-white px-4 py-2 text-xs font-bold lg:px-5 lg:py-3 lg:text-base">
+            Sign In
+          </button>
+        </header>
+        <hr className="my-2 border" />
+        <div className="mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:space-y-0 lg:justify-center">
+          <img
+            className="w-80 object-cover pb-10 lg:h-40"
+            src="https://links.papareact.com/bdy"
+            alt=""
+          />
+          <h1 className="text-3xl font-bold lg:text-5xl lg:font-extrabold">
+            The PAPAFAM Ape Coding Club | NFT Drop
+          </h1>
+          <p className="pt-2 text-xl text-green-500">13 / 21 NFT's claimed</p>
+        </div>
+        <button className="h-16 w-full bg-red-600 text-white rounded-full mt-10 font-bold">
+          Mint NFT (0.01 ETH)
+        </button>
+      </main>
     </div>
   )
 }
